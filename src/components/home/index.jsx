c403d5514abf96c01c76779060bac724af98491f
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-11-12 19:05:10 +0100

    Add the metabnb nft section

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -2,6 +2,7 @@ import Header from '../common/Header';
 import HouseCards from './HouseCards.jsx';
 import HouseCardsHeader from './HouseCardsHeader';
 import Intro from './Intro';
+import MetaBnbNFT from './MetaBnbNFT';
 import PartnerLogo from './PartnerLogo';
 
 const Home = () => (
@@ -11,6 +12,7 @@ const Home = () => (
     <PartnerLogo />
     <HouseCardsHeader />
     <HouseCards />
+    <MetaBnbNFT />
   </div>
 );
 export default Home;
