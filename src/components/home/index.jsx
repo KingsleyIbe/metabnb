7a600fd53318a9d7916a3fa96ded79c3c3340929
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-11-15 10:10:07 +0100

    Fix bugs with modal popup

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ConnectWallet from '../common/ConnectWallet';
 import Footer from '../common/Footer';
 import Header from '../common/Header';
 import MobileHeader from '../common/MobileHeader';
@@ -8,21 +9,31 @@ import Intro from './Intro';
 import MetaBnbNFT from './MetaBnbNFT';
 import PartnerLogo from './PartnerLogo';
 
-const Home = () => (
-  <>
-    <div className="relative">
-      <Header />
-      <MobileHeader />
-      <div>
-        <Intro />
-        <PartnerLogo />
-        <HouseCardsHeader />
-        <HouseCards />
-        <MetaBnbNFT />
-        <Footer />
-      </div>
-    </div>
-  </>
-);
+const Home = () => {
+  const [word, setWord] = useState(false);
+  const openModal = () => {
+    setWord(!word);
+    // localStorage.setItem('modalState', isOpen);
+    // console.log(isOpen);
+  };
 
+  return (
+
+    <>
+      <div className={`${word ? 'opacity-[0.6]' : 'opacity-[1]'} relative hidden p-10 lg:block`}>
+        <Header changeWord={openModal} />
+        <MobileHeader />
+        <div className={`${word ? 'opacity-[0.6]' : 'opacity-[1]'}`}>
+          <Intro />
+          <PartnerLogo />
+          <HouseCardsHeader />
+          <HouseCards />
+          <MetaBnbNFT />
+          <Footer />
+        </div>
+      </div>
+      {word ? (<div className="relative"><ConnectWallet /></div>) : ''}
+    </>
+  );
+};
 export default Home;
